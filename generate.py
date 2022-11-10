144a3002e5490b9745ae2335126158fc28e0183e
alpal <>
2022-11-10 12:45:55 +0000

    Formatting with black

--- generate.py
+++ generate.py
@@ -8,17 +8,17 @@ uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('len', type=int,
-                    help='password length')
-parser.add_argument('-n', '--numbers', action='store_true',
-                    help='exclude numbers')
-parser.add_argument('-s', '--symbols', action='store_true',
-                    help='exclude symbols')
+    help='password length')
+parser.add_argument('-n', '--numbers',   action='store_true',
+    help='exclude numbers')
+parser.add_argument('-s', '--symbols',   action='store_true',
+    help='exclude symbols')
 parser.add_argument('-l', '--lowercase', action='store_true',
-                    help='exclude lowercase characters')
+    help='exclude lowercase characters')
 parser.add_argument('-u', '--uppercase', action='store_true',
-                    help='exclude uppercase characters')
+    help='exclude uppercase characters')
 parser.add_argument('-e', '--exclude', type=str,
-                    help='set of characters to exclude')
+    help='set of characters to exclude')
 args = parser.parse_args()
 
 if args.numbers:
